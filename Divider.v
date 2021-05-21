8a3bf50b44bc546da4992ca11848f6fbc411c97d
Daydreamer97 <81462919+Daydreamer97@users.noreply.github.com>
2021-05-21 23:53:15 +0800

    Update Divider.v

--- Divider.v
+++ Divider.v
@@ -2,21 +2,21 @@ module CP_1kHz_1Hz(nRST,CLK_50,_1Hz,_10Hz,_100Hz,_1kHz);
 	input CLK_50,nRST;
 	output _1Hz,_10Hz,_100Hz,_1kHz;
 	Divider50MHz U0(.CLK_50M(CLK_50),.nCLR(nRST),.CLK_1HzOut(_1KHz));
-	defparam U2.N=15,
-				U2.CLK_Freq=50000000,
-				U2.OUT_Freq=1000;
+	defparam U0.N=15,
+				U0.CLK_Freq=50000000,
+				U0.OUT_Freq=1000;
 	Divider50MHz U1(.CLK_50M(CLK_50),.nCLR(nRST),.CLK_1HzOut(_100Hz));
-	defparam U3.N=18,
-				U3.CLK_Freq=50000000,
-				U3.OUT_Freq=100;
+	defparam U1.N=18,
+				U1.CLK_Freq=50000000,
+				U1.OUT_Freq=100;
 	Divider50MHz U2(.CLK_50M(CLK_50),.nCLR(nRST),.CLK_1HzOut(_10Hz));
-	defparam U4.N=22,
-				U4.CLK_Freq=50000000,
-				U4.OUT_Freq=10;
+	defparam U2.N=22,
+				U2.CLK_Freq=50000000,
+				U2.OUT_Freq=10;
 	Divider50MHz U3(.CLK_50M(CLK_50),.nCLR(nRST),.CLK_1HzOut(_1Hz));
-	defparam U5.N=25,
-				U5.CLK_Freq=50000000,
-				U5.OUT_Freq=1;
+	defparam U3.N=25,
+				U3.CLK_Freq=50000000,
+				U3.OUT_Freq=1;
 endmodule
 
 module Divider50MHz(CLK_50M,nCLR,CLK_1HzOut);
